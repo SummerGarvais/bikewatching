d13a2b1062ce80f8617d99411daf233e0a73a43f
SummerGarvais <garvaiskevin@gmail.com>
2026-05-15 12:28:22 -0700

    Update map.js

--- map.js
+++ map.js
@@ -3,8 +3,8 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-// Mapbox access token
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VtbWVyLWdhcnZhaXMiLCJhIjoiY21wNzdpdnphMDNwbTMyb2p6OTFjZmVjNyJ9.MAon4NCuMzf4EbK7DRtF_Q'
+// Mapbox access token specifically for this website
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3VtbWVyLWdhcnZhaXMiLCJhIjoiY21wN2I1NzEwMDBhZDJzcHNlZWc5Y2wyYSJ9.HMU2mwjdXKBuu0ddWufQmA'
 
 // Initialize the map
 const map = new mapboxgl.Map({
