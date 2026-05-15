56786e3dcdee7b66d5a29dc2b7fb6a1daff2993e
SummerGarvais <garvaiskevin@gmail.com>
2026-05-15 12:47:39 -0700

    Updated up until 1.5

--- map.js
+++ map.js
@@ -3,8 +3,8 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-// Mapbox access token specifically for this website
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VtbWVyLWdhcnZhaXMiLCJhIjoiY21wN2I1NzEwMDBhZDJzcHNlZWc5Y2wyYSJ9.HMU2mwjdXKBuu0ddWufQmA'
+// Mapbox access token (CHANGE TO BE specifically for this website)
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3VtbWVyLWdhcnZhaXMiLCJhIjoiY21wN2JobGFsMDBlcjJzcHM0dHozZnlqciJ9.qx0aeS-smu7i5rdS4hkHSA'
 
 // Initialize the map
 const map = new mapboxgl.Map({
@@ -12,6 +12,6 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/streets-v12', // Map style
     center: [-71.09415, 42.36027], // [longitude, latitude]
     zoom: 12, // Initial zoom level (adjustable)
-    minZoom: 5, // Adjustable
+    minZoom: 9, // Adjustable
     maxZoom: 18, // Adjustable
 });
